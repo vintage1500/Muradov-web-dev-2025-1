df388fbfef37fa24399727ea12fc6c0a22d38079
raul <iam@gmail.com>
2025-03-20 15:40:23 +0300

    do lab and finally do tests

--- lab1/app/app.py
+++ lab1/app/app.py
@@ -1,11 +1,13 @@
 import random
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from faker import Faker
 
 fake = Faker()
 
 app = Flask(__name__)
 application = app
+
+app.config["SERVER_NAME"] = '127.0.0.1:5000'
 
 images_ids = ['7d4e9175-95ea-4c5f-8be5-92a6b708bb3c',
               '2d2ab7df-cdbc-48a8-a936-35bba702def5',
@@ -44,8 +46,10 @@ def posts():
 
 @app.route('/posts/<int:index>')
 def post(index):
+    if index < 0 or index >= len(posts_list):
+        abort(404)
     p = posts_list[index]
-    return render_template('post.html', title=p['title'], post=p)
+    return render_template('post.html', title=p['title'], post=p) 
 
 @app.route('/about')
 def about():
