631844176fefe29f07849c76d5d19fe3c503f87c
raul <myemail@mail.ru>
2025-03-27 09:59:52 +0300

    fix ug footer in 1 lab and start do 2 lab

---
+++ lab2/app/tests/test-posts.py
@@ -0,0 +1,83 @@
+import pytest
+from flask import url_for
+from app import app, posts_list
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        with app.app_context():
+            yield client
+
+def test_index_page_status_code(client):
+    response = client.get(url_for('index'))
+    assert response.status_code == 200
+
+def test_index_page_html(client):
+    response = client.get(url_for('index')) 
+    assert b'<!doctype html>' in response.data
+
+def test_about_page_status_code(client):
+    response = client.get(url_for('about'))
+    assert response.status_code == 200 
+
+def test_about_page_html(client):
+    response = client.get(url_for('about')) 
+    assert b'<!doctype html>' in response.data
+
+def test_about_page_html_tag(client):
+    response = client.get(url_for('about')) 
+    response_text = response.data.decode('utf-8').strip()
+    assert '<h1 class="mt-5 text-center">Об авторе</h1>' in response_text
+
+def test_posts_page_status_code(client):
+    response = client.get(url_for('posts'))
+    assert response.status_code == 200  
+
+def test_posts_page_html(client):
+    response = client.get(url_for('posts')) 
+    assert b'<!doctype html>' in response.data
+
+def test_posts_page_html_tag(client):
+    response = client.get(url_for('posts')) 
+    response_text = response.data.decode('utf-8').strip()
+    assert '<h1 class="my-5">Последние посты</h1>' in response_text
+
+def test_post_page_status_code(client):
+    response = client.get(url_for('post', index=0))
+    assert response.status_code == 200
+
+def test_post_page_html(client):
+    response = client.get(url_for('post', index=0))
+    assert b'<!doctype html>' in response.data
+
+def test_page_post_comment_title(client): 
+    response = client.get(url_for('post', index=1)) 
+    response_text = response.data.decode('utf-8').strip()
+    assert 'Оставьте комментарий' in response_text
+
+def test_posts_page_title(client):
+    response = client.get(url_for('posts'))
+    for post in posts_list:
+        assert post['title'].encode() in response.data
+
+def test_post_page_data(client):
+    post = posts_list[0]
+    response = client.get(url_for('post', index=0)) 
+    assert post['title'].encode() in response.data
+    assert post['text'][:50].encode() in response.data
+    assert post['author'].encode() in response.data
+
+def test_non_existent_post(client):
+    response = client.get(url_for('post', index=999))
+    assert response.status_code == 404
+
+def test_post_image_loading(client):
+    post = posts_list[0]
+    response = client.get(url_for('post', index=0)) 
+    assert f'src="{url_for("static", filename="images/" + post["image_id"])}"'.encode() in response.data
+
+def test_redirect_on_invalid_url(client):
+    response = client.get('/some_invalid_url')
+    assert response.status_code == 404  
+
