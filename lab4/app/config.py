fa36f1f0c8e5ac6bced7c3e58cf10d909db99b80
raul <myemail@mail.ru>
2025-05-24 17:34:26 +0300

    continue try to host 2

--- lab4/app/config.py
+++ lab4/app/config.py
@@ -6,6 +6,6 @@ SECRET_KEY = '8806d05fdb32c6b25bbe417def4258c5e9b4dc4d865aa57a66105ce119d3da2e'
 # MYSQL_DATABASE = 'flask_user' 
 
 MYSQL_USER = 'vintage150'
-MYSQL_PASSWORD = '12345678'
+MYSQL_PASSWORD = 'qwerty123!'
 MYSQL_HOST = 'vintage150.mysql.pythonanywhere-services.com'
 MYSQL_DATABASE = 'vintage150$default'
