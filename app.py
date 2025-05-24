46cc1e2ac2c556047948a2e2fd6e41df666e4267
raul <myemail@mail.ru>
2025-05-24 17:20:46 +0300

    continue try to host

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from lab1.app.app import app as lab1_app 
 from lab2.app.app import app as lab2_app
 from lab3.app.app import app as lab3_app
+from lab4.app import create_app
 from root_app.app.app import app as root_app
 
 app = DispatcherMiddleware(root_app, {
     '/lab1': lab1_app,
     '/lab2': lab2_app,
-    '/lab3': lab3_app
+    '/lab3': lab3_app,
+    '/lab4': create_app()
 })
 application = app
