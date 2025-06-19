51251c841fccb9ec2c1eca9785367af1fc718363
raul <myemail@mail.ru>
2025-06-19 18:21:28 +0300

    add dispatcher middleware

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab3.app.app import app as lab3_app
 from lab4.app import create_app as lab4_app
 from lab5.app import create_app as lab5_app
 from lab6.app import create_app as lab6_app
+from course_work.app import create_app as course_work_app
 from root_app.app.app import app as root_app
 
 app = DispatcherMiddleware(root_app, {
@@ -13,6 +14,7 @@ app = DispatcherMiddleware(root_app, {
     '/lab3': lab3_app,
     '/lab4': lab4_app(),
     '/lab5': lab5_app(),
-    '/lab6': lab6_app()
+    '/lab6': lab6_app(),
+    '/course_work': course_work_app()
 })
 application = app
